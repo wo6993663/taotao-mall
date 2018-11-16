1cf28d63cc93ae7bd2099614f47b2d5cba565192
wo6738663 <821548593@qq.com>
2018-11-16 09:40:44 +0800

    前台展示项目

---
+++ src/main/java/com/taotao/portal/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.taotao.portal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.taotao.common.pojo.TaotaoResult;
+
+@Controller
+public class IndexController {
+	
+
+
+	@RequestMapping("/index")
+	public String showIndex(Model model) {
+		return "index";
+	}
+	
+	@RequestMapping(value="/httpclient/post", method=RequestMethod.POST, 
+			produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
+	@ResponseBody
+	public String testPost(String username, String password) {
+		String result = "username:" + username + "\tpassword:" + password;
+		System.out.println(result);
+		return "username:" + username + ",password:" + password;
+	}
+}
