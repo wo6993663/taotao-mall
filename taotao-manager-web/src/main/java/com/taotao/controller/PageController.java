814eec1a478238d512841cb4dcfb5a48ccc0814f
wo6738663 <821548593@qq.com>
2018-11-16 09:31:36 +0800

    后台管理聚合项目

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,33 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转controller
+ * <p>Title: PageController</p>
+ * <p>Description: </p>
+ */
+@Controller
+public class PageController {
+
+	/**
+	 * 打开首页
+	 */
+	@RequestMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+	/**
+	 * 展示其他页面
+	 * <p>Title: showpage</p>
+	 * <p>Description: </p>
+	 * @param page
+	 * @return
+	 */
+	@RequestMapping("/{page}")
+	public String showpage(@PathVariable String page) {
+		return page;
+	}
+}
